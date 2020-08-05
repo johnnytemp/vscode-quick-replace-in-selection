b515b71b3eb8272ce4f099fa4df7bacb0ee75e76
Johnny Wong <johnny_temp@yahoo.com.hk>
2020-08-06 02:28:34 +0800

    Sort the order of rules by name when shown in the picker.

--- src/QuickReplaceInSelectionConfig.ts
+++ src/QuickReplaceInSelectionConfig.ts
@@ -26,16 +26,19 @@ export class QuickReplaceInSelectionConfig {
 
     let configRules: QuickReplaceRulesFalseAble = vsConfig.get("rules") || {};
     this._rules = {};
-    for (let key in configRules) {
+    let names = Object.keys(configRules);
+    names.sort();   // sort by rule names
+    // for (let key in configRules) {
+    names.forEach(key => {
       let rule = configRules[key];
       // Remark: use `"Rule name": false` to delete a rule explicity
       if (rule === false || (rule['find'] === undefined || rule['replace'] === undefined))
-        continue;
+        return;
       if (typeof rule['find'] === 'string' && typeof rule['replace'] === 'string') {
         rule = Object.assign({}, rule, { find: [rule.find], replace: [rule.replace] });
       }
       this._rules[key] = rule;
-    }
+    });
     return vsConfig;
   }
 }
